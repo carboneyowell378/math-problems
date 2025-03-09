f06a9d64bbc626b286134c224d148c30d83242e1
carboneyowell378 <carboneyowell378@gmail.com>
2025-03-09 17:11:46 +0000

    Add 4.ts

---
+++ src/4.ts
@@ -0,0 +1,16 @@
+import random
+
+def get_random_math_problem():
+    num1 = random.randint(0, 10)
+    num2 = random.randint(0, 10)
+    op = random.choice(['+', '-', '*', '/'])
+    if op == '+':
+        return f'{num1} + {num2}'
+    elif op == '-':
+        return f'{num1} - {num2}'
+    elif op == '*':
+        return f'{num1} * {num2}'
+    else:
+        return f'{num1} / {num2}'
+
+print(get_random_math_problem())
